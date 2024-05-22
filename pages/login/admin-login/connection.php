0c4428e3394940dcd087e1b9c3fb1d0e2e3bcef8
S22007412 <zS22007412@estudiantes.uv.mx>
2024-05-22 09:26:14 -0600

    php test

--- pages/login/admin-login/connection.php
+++ pages/login/admin-login/connection.php
@@ -1,22 +1,20 @@
 <?php
-$serverName = "localhost";
-$username = "d1rdrd";
-$password = "ubiuv.UV1!";
-$database = "ubiuv";
+// Database connection parameters
+$servername = "localhost"; // Change this to your database server name
+$username = "d1rdrd"; // Change this to your database username
+$password = "ubiuv.UV1!"; // Change this to your database password
+$database = "ubiuv"; // Change this to your database name
 
 // Create connection
-$conn = new mysqli($serverName, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . formatErrors($conn->connect_error));
+    die("Connection failed: " . $conn->connect_error);
 }
 
-function formatErrors($error)
-{
-    // Display errors
-    echo "<h1>SQL Error:</h1>";
-    echo "Error information: <br/>";
-    echo "Message: " . $error . "<br/>";
-}
+echo "Connected successfully";
+
+// Close connection
+$conn->close();
 ?>
