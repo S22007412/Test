f83cb3c98dab104071afd981359b383b8769501b
S22007412 <zS22007412@estudiantes.uv.mx>
2024-05-22 09:33:28 -0600

    php test

--- php/connection.php
+++ php/connection.php
@@ -4,6 +4,15 @@ $username = "d1rdrd";
 $password = "ubiuv.UV1!";
 $database = "ubiuv";
 
+// Define the formatErrors function before it is used
+function formatErrors($error)
+{
+    // Display errors
+    echo "<h1>SQL Error:</h1>";
+    echo "Error information: <br/>";
+    echo "Message: " . $error . "<br/>";
+}
+
 // Create connection
 $conn = new mysqli($serverName, $username, $password, $database);
 
@@ -12,11 +21,5 @@ if ($conn->connect_error) {
     die("Connection failed: " . formatErrors($conn->connect_error));
 }
 
-function formatErrors($error)
-{
-    // Display errors
-    echo "<h1>SQL Error:</h1>";
-    echo "Error information: <br/>";
-    echo "Message: " . $error . "<br/>";
-}
+echo "Connected successfully";
 ?>
