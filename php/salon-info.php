10bbd56834d5f2af23b75bc948626e4ec9177738
ZS22007412 <zs22007412@estudiantes.uv.mx>
2024-05-25 17:28:51 -0600

    php test

--- php/salon-info.php
+++ php/salon-info.php
@@ -3,14 +3,6 @@ include '../../../../php/connection.php'; // Ensure this includes the correct co
 
 // Assuming you have variables $id_edificio and $id_salon defined somewhere earlier
 
-// Create a connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
 // Prepare the SQL statement
 $id_edificio = mysqli_real_escape_string($conn, $id_edificio);
 $id_salon = mysqli_real_escape_string($conn, $id_salon);
