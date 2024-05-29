eb3da1383da4bbd22cf7d638d2ea1c3d97712d7b
ZS22007412 <zs22007412@estudiantes.uv.mx>
2024-05-28 23:07:46 -0600

    js test

--- scripts/upload-form-visibility.js
+++ scripts/upload-form-visibility.js
@@ -6,11 +6,11 @@
                                 form.style.display = 'block';                
                                 // document.querySelector('.modify-container').style.marginTop = '-8vh'; // Adjust the value as needed
                                 //document.querySelector('.modify-container').style.height = '12vh'; // Reset to the original value
-                                document.querySelector('.edificio-img').style.marginBottom = '-4vh'; // Adjust the value as needed
+                                //document.querySelector('.edificio-img').style.marginBottom = '-4vh'; // Adjust the value as needed
                             } else {
                                 form.style.display = 'none';
                                 //document.querySelector('.modify-container').style.marginTop = '-8vh'; // Adjust the value as needed
                                 // document.querySelector('.modify-container').style.height = '3vh'; // Reset to the original value
-                                document.querySelector('.edificio-img').style.marginBottom = '-4vh';
+                                //document.querySelector('.edificio-img').style.marginBottom = '-4vh';
                             }
                         });
