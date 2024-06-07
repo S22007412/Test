c3681192171484e8b8d7d8573582a0c7eaa97f32
ZS22007412 <zs22007412@estudiantes.uv.mx>
2024-06-07 16:29:06 -0600

    changed url

--- edificios/edificio-a/index.php
+++ edificios/edificio-a/index.php
@@ -329,11 +329,8 @@ $relative_target_file = isset($image_paths[$page_tag]) ? $image_paths[$page_tag]
                         <script>
                             document.getElementById('download-button-1').addEventListener('click', function() {
 
-                                if (navigator.userAgent.indexOf('median') > -1) {
-                                    median.share.downloadFile({url: '<?php echo htmlspecialchars($schedulesDownload['a-15-matutino']); ?>', open: false})}
-                                else {
+                                
                                     window.location.href = '<?php echo htmlspecialchars($schedulesDownload['a-15-matutino']); ?>';
-                                }
                             });
 
                             document.getElementById('download-button-2').addEventListener('click', function() {
